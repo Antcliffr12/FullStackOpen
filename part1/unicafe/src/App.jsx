884aebbd0a872bad6b42a065b0f341db746a45ee
Ryan Antcliff <antcliffr12@gmail.com>
2024-04-14 19:46:45 -0400

    completed props of stats

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,6 +3,27 @@ import { useState } from 'react'
 
 const Button = ({onClick, text }) => <button onClick={onClick}>{text}</button>
 
+const Statistics = (props) => {
+  return(
+    <>
+     <h2>{props.title}</h2>
+      <ul>
+        <li>Good: {props.good}</li>
+        <li>Neutral: {props.neutral}</li>
+        <li>Bad: {props.bad}</li>
+        <li>Total: {props.total}</li>
+        <li>Avg: {props.average}</li>
+        <li>Positive: {props.positive} %</li> 
+        {/* <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>Avg: {average/total}</li>
+        <li>Positive: {(good/total) * 100} %</li> */}
+      </ul>
+    </>
+  )
+}
+
 function App() {
   const [ good, setGood ] = useState(0);
   const [ neutral, setNeutral] = useState(0);
@@ -41,15 +62,15 @@ function App() {
       <Button onClick={handleNeutralClicks} text="neutral" />
       <Button onClick={handleBadClicks} text="bad" />
       <br/> 
-      <h2>Statistics</h2>
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Avg: {average/total}</li>
-        <li>Positive: {(good/total) * 100} %</li>
-      </ul>
+      <Statistics 
+        title="Statistics" 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        total={total} 
+        average={average/total} 
+        positive={(good/total) * 100} 
+      />
     </div>
   )
 }
