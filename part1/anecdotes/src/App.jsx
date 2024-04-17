ab2ed62fbebbb64d555b0f4393c64d400571e6fd
Ryan Antcliff <antcliffr12@gmail.com>
2024-04-16 21:44:43 -0400

    finsihed part1

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -16,15 +16,42 @@ function App() {
     'The only way to go fast, is to go well.'
   ]
   const [selected, setSelected] = useState(0)
-   
+  const initVotes = Array(anecdotes.length).fill(0);
+  const [votes, setVotes] = useState(initVotes);
+
   const handleAnecdotes = () => {
     const randomAnecdotes = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomAnecdotes)
   }
+
+  const handleVotes = () => {
+    const copyVotes = [...votes];
+    copyVotes[selected] += 1;
+    setVotes(copyVotes);
+  }
+  
+  //loop thorugh arry of votes to get the highest
+  //get key of highest Vote and input it with word list
+  let highestVoteIndex = 0;
+  for (let i = 1; i < votes.length; i++) {
+    if (votes[i] > votes[highestVoteIndex]) {
+      highestVoteIndex = i;
+    }
+  }
+ 
   return (
     <div>
       {anecdotes[selected]}
       <div>
+        has votes {votes[selected]}
+      </div>  
+      <div>
+        <h2>Anecdote with the most votes</h2>
+
+        {anecdotes[highestVoteIndex]}
+      </div>
+      <div>
+        <Button onSmash={handleVotes} text="Vote" />
         <Button onSmash={handleAnecdotes} text="Next Anecdote" />
       </div>
     </div>
