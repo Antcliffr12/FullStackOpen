20d063d75726d12625986020cfa81a38997890ee
Ryan Antcliff <antcliffr12@gmail.com>
2024-05-25 17:13:42 -0400

    Countries API search

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,61 +1,58 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Country from "./Components/Country";
 
-function App() {
-	const [countries, setCountries] = useState(null);
+const App = () => {
 	const [searchCountries, setSearchCountries] = useState("");
-	const [country, setCountry] = useState(null);
-
-	const getAllCountries = () => {
-		axios
-			.get("https://studies.cs.helsinki.fi/restcountries/api/all")
-			.then((response) => {
-				setCountries(response.data);
-			});
-	};
-
-	const getCountryByName = () => {
-		axios
-			.get("https://studies.cs.helsinki.fi/restcountries/api/name/sweden")
-			.then((response) => {
-				console.log(response.data, "data");
-			});
-	};
+	const [countries, setCountries] = useState(null);
+	const [country, setCountry] = useState({});
 
 	useEffect(() => {
-		getAllCountries();
-	}, []);
+		console.log("effect run, country is now", countries);
 
-	if (!countries) {
-		return null;
-	}
+		if (countries) {
+			axios
+				.get(
+					`https://studies.cs.helsinki.fi/restcountries/api/name/${countries}`
+				)
+				.then((response) => {
+					console.log(response.data);
+					setCountry(response.data);
+				})
+				.catch((error) => {
+					console.log("Could not fetch countries", error);
+				});
+		}
+	}, [countries]);
 
-	const searchInput = (event) => {
+	const handleChange = (event) => {
 		setSearchCountries(event.target.value);
 	};
-	const searchFilter = countries.filter((country) => {
-		return country.name.common
-			.toLowerCase()
-			.includes(searchCountries.toLowerCase());
-	});
 
+	const onSearch = (event) => {
+		event.preventDefault();
+		setCountries(searchCountries);
+	};
+
+	let name = "";
+	let flag = "";
+	let languages = [];
+	if (country) {
+		name = country.name?.common;
+		flag = country.flag;
+	}
 	return (
 		<div>
-			find countries: <input onChange={searchInput} />
-			{searchFilter.length === 1 ? (
-				<p>Matching 1 Country</p>
-			) : searchFilter.length >= 10 ? (
-				<p>Too many matches, specify another filter</p>
-			) : searchFilter.length >= 1 ? (
-				searchFilter.map((country) => {
-					return <Country key={country.cca2} name={country.name.common} />;
-				})
-			) : (
-				<p>No matches found</p>
-			)}
+			<form onSubmit={onSearch}>
+				<input type="text" value={searchCountries} onChange={handleChange} />
+				<button type="submit">Country</button>
+			</form>
+			<h1>{name}</h1>
+			<h2>{flag}</h2>
+			{country.language?.map((language, i) => {
+				return <li key={i}>{language}</li>;
+			})}
 		</div>
 	);
-}
+};
 
 export default App;
